886da455869addfa5b0f5da773ca92396d6c3600
danielleboz <daniellekbosworth@gmail.com>
2021-07-17 13:52:16 -0400

    added parent button to header

--- client-side/js/Components/Header.js
+++ client-side/js/Components/Header.js
@@ -1,10 +1,23 @@
 export default function Header(){
     return`
+    <div class="header__section_parents">
+        <button class="header__parents_button">Grownups</button>
+        <div class="header__parentsMenu_backdrop"></div>
+        <div class="header__parentsBtn_content">
+            <ul class="header__parents_menu">
+                <li class="header__parents_aboutUs">About Us</li>
+                <li class="header__parent_contactUs">Contact Us</li>
+                <li class="header__parent_faq">FAQ</li>
+                <li class="header__parent_terms">Terms & Conditions</li>
+                <li class="header__parent_privacy">Privacy Policy</li>
+            </ul>
+        </div>
+    </div>
     <ul class = "nav-list">
-    <li class="nav-list__home">Home</li>
-    <li class="nav-list__students">Students</li>
-    <li class="nav-list__parents">Parents</li>
-    <li class="nav-list__learning">Learning</li>
+        <li class="nav-list__home">Home</li>
+        <li class="nav-list__grades">Grades 5-8</li>
+        <li class="nav-list__games">Games</li>
+        <li class="nav-list__learning">Learning</li>
     </ul>
     `;
 }
