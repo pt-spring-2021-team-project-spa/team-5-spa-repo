393d16242544f5cba2d622a4ef4b9768c02c3512
jordaneaster <jordaneaster222@icloud.com>
2021-07-22 03:17:13 -0400

    sapce0invader-test

--- client-side/js/Components/Grades14.js
+++ client-side/js/Components/Grades14.js
@@ -1,5 +1,35 @@
-export default function Grades14(){
-    return `
-    <h1>Welcome Student!</h1>
+import spaceButtonLogic from "../space_invader_game/spaceButtonLogic";
+import starField from "../space_invader_game/starField";
+import spaceLogic from "../space_invader_game/spaceInvaderLogic";
+
+export default function Grades14() {
+  return `
+    <html>
+    <head>
+    <title>Space Invaders</title>
+    <link
+      rel="stylesheet"
+      type="css"
+      href="client-side/css/spaceinvaderStyle.css"
+    />
+  </head>
+  <body>
+    <div id="starfield"></div>
+    <div id="gamecontainer">
+      <canvas id="gameCanvas"></canvas>
+    </div>
+    <div id="info">
+      <p>
+        Move with arrow keys or swipe, fire with the space bar or touch. The
+        invaders get faster and drop more bombs as you complete each level!
+      </p>
+    </div>
+
+    <script type='module' src="client-side/js/space_invader_game/starField.js"></script>
+    <script type='module' src="client-side/js/space_invader_game/spaceButtonLogic.js"></script>
+    <script type='module' src="client-side/js/space_invader_game/spaceInvaderLogic.js"></script>
+  </body>
+    </html>
+    
     `;
 }
