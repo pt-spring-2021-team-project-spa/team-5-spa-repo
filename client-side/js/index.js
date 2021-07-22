73d2a74a02e9158581aaee4b06125cfc290adc28
roseholmesminton <rose.yvonne216@icloud.com>
2021-07-22 16:18:53 -0400

    Female Art works

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -168,27 +168,67 @@ function navFemaleArt() {
   femaleArtElem.addEventListener("click", () => {
     const app = document.querySelector("#app");
     const getWomanUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=woman"
-const apiLink = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
+    const apiLink = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
 
-fetch(`${getWomanUrl}`)  
-  .then(response => response.json())
-  .then(data => {
-    console.log('1:', data.objectIDs);
-    return data;
-  })
-  .then(data => {
-    let objectIDs = data.objectIDs; 
-    let objectID = objectIDs[Math.floor(Math.random() * objectIDs.length)]; // Select a random Id
-    console.log(objectID);
-
-    fetch(`${apiLink}` + objectID) 
-      .then(resp => resp.json())
+    fetch(`${getWomanUrl}`)
+      .then(response => response.json())
       .then(data => {
-        console.log(data);
-    
-          });
+        return data;
       })
-      
-  })
+      .then(data => {
+        let objectIDs = data.objectIDs;
+        let objectID = objectIDs[Math.floor(Math.random() * objectIDs.length)]; // Select a random Id
+        console.log(objectID);
 
-  }
+        fetch(`${apiLink}` + objectID)
+          .then(resp => resp.json())
+          .then(data => {
+            console.log(data);
+            app.innerHTML = FemaleArt(data);
+      
+            let title = document.querySelector(".title");
+            title.innerHTML = `${data.title} `;
+
+            let department = document.querySelector('.department');
+            department.innerHTML = `Department: ${data.department} `
+
+            if (`${data.culture}` === '') {
+            } else {
+              let culture = document.querySelector('.culture');
+              culture.innerHTML = `Culture: ${data.culture} `
+            }
+
+            if (`${data.period}` === '') {
+            } else {
+              let period = document.querySelector('.period');
+              period.innerHTML = `Period: ${data.period}`
+            }
+
+            let objectEndDate = document.querySelector('.objectEndDate');
+            objectEndDate.innerHTML = `Date: ${data.objectEndDate} `
+
+            let primaryImage = document.querySelector(".photo");
+            primaryImage.innerHTML = `<img src="${data.primaryImage}" class="primaryImage" > `;
+
+            let artistDisplayName = document.querySelector('.artistDisplayName');
+            artistDisplayName.innerHTML = `${data.artistDisplayName} `
+
+            let artistNationality = document.querySelector('.artistNationality');
+            artistNationality.innerHTML = `${data.artistNationality} `
+
+            let artistDisplayBio = document.querySelector('.artistDisplayBio');
+            artistDisplayBio.innerHTML = `${data.artistDisplayBio} `
+
+          })
+    
+        let button = document.getElementById('nextButton')
+        button.addEventListener('click', () => {
+          window.setTimeout(() => {
+            window.location.reload(true)
+          }, 200)
+        })
+      
+      });
+  
+  })
+}
