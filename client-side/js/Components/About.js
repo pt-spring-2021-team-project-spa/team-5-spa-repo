b329b028e08d5f2c902f85acd95174aa51e7cfae
FilsanMadar <filsanmadar@gmail.com>
2021-07-22 22:50:27 -0400

    moved code to component js file

--- client-side/js/Components/About.js
+++ client-side/js/Components/About.js
@@ -1,6 +1,74 @@
-
-export default function About(){
-    return`
-    <h1>About Test Page</h1>
+export default function About() {
+  return `
+    <div class="team-container">
+      <h1>Our Team</h1>
+      <div class="team5">
+        <div class="team-member">
+          <div class="team_img">
+            <img src="client-side/assets/adam.jpeg" alt="Adam_img" />
+          </div>
+          <h3>Adam Heinemen</h3>
+          <p class="role">Full Stack Developer</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+            aperiam facilis modi sit velit fugit possimus, consectetur eos
+            accusamus labore maiores asperiores eum animi laudantium quo
+            voluptatibus iste vero cum!
+          </p>
+        </div>
+        <div class="team-member">
+          <div class="team_img">
+            <img src="client-side/assets/dbosworth.png" alt="Danielle_img" />
+          </div>
+          <h3>Danielle Bosworth</h3>
+          <p class="role">Full Stack Developer</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            consequatur eveniet, provident laudantium temporibus quasi, voluptas
+            sit neque adipisci beatae illum iusto at sed doloribus, ipsa porro!
+            Itaque, omnis provident.
+          </p>
+        </div>
+        <div class="team-member">
+          <div class="team_img">
+            <img src="client-side/assets/filsan.jpg" alt="Filsan_img" />
+          </div>
+          <h3>Filsan Madar</h3>
+          <p class="role">Full Stack Developer</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            quos aliquam, minima deserunt magni perferendis, sunt assumenda
+            adipisci fugiat sint quod voluptatibus non praesentium enim
+            doloremque veritatis, nisi illo animi.
+          </p>
+        </div>
+        <div class="team-member">
+          <div class="team_img">
+            <img src="client-side/assets/jordan.jpeg" alt="Jordan_img" />
+          </div>
+          <h3>Jordan Easter</h3>
+          <p class="role">Full Stack Developer</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat sed
+            natus accusantium quidem fugit libero doloremque earum nostrum
+            veritatis odio laborum voluptatibus deserunt porro officia corporis,
+            cum autem ipsam quasi.
+          </p>
+        </div>
+        <div class="team-member">
+          <div class="team_img">
+            <img src="client-side/assets/rose.jpeg" alt="Rose_img" />
+          </div>
+          <h3>Rose Holmes-Minton</h3>
+          <p class="role">Full Stack Developer</p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe
+            doloribus quia quidem ullam non cum voluptatibus molestiae dolor
+            ipsa exercitationem totam quod ipsum deleniti praesentium, nostrum
+            sunt dicta quos odio.
+          </p>
+        </div>
+      </div>
+    </div>
 `;
-    }
+}
