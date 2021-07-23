89d1e4c1e23a67cc89d3829d909b383f6e3c0483
adamh33 <athdesign89@gmail.com>
2021-07-23 15:47:39 -0400

    added blastoff button back, working on home page

--- client-side/js/Components/Home.js
+++ client-side/js/Components/Home.js
@@ -1,9 +1,8 @@
-import planetImg from "../../assets/planetsCard.jpg";
+
 export default function Home() {
   return `
   <div id="home-div">
-    <h1>Home Page Test</h1>
-    <img class="planetCardImg" src='${planetImg}'>
+    <h1>JacKr Learning</h1>
     </div>
     `;
 }
