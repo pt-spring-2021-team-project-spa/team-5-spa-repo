b859673b44eb916a2fd50fd39709e1ea4091d02e
adamh33 <athdesign89@gmail.com>
2021-07-23 20:38:23 -0400

    cards changed

--- client-side/js/Components/Grades58.js
+++ client-side/js/Components/Grades58.js
@@ -1,4 +1,7 @@
 import planetImg from "../../assets/planetsCard.jpg";
+import spaceImg from "../../assets/space.jpg";
+import earthImg from "../../assets/earth.jpg";
+import questionImg from "../../assets/question.jpg";
 export default function Grades58() {
   return `
     <h1>Our Solar System</h1>
@@ -6,22 +9,22 @@ export default function Grades58() {
     <div class="card">
     <img class="planetCardImg" src='${planetImg}'>
     <h3>Planets</h3>
-    <p class="cardInfo1">Quick Overview of planets category</p>
+    <p class="cardInfo1">Click Here!</p>
     </div>
     <div class="card">
-    <img class="planetCardImg" src='${planetImg}'>
+    <img class="planetCardImg" src='${spaceImg}'>
     <h3>Space</h3>
-    <p class="cardInfo2">Quick Overview of planets category</p>
+    <p class="cardInfo2">Click Here!</p>
     </div>
     <div class="card">
-    <img class="planetCardImg" src='${planetImg}'>
+    <img class="planetCardImg" src='${earthImg}'>
     <h3>Blue Marble</h3>
-    <p class="cardInfo3">Quick Overview of planets category</p>
+    <p class="cardInfo3">Click Here!</p>
     </div>
     <div class="card">
-    <img class="planetCardImg" src='${planetImg}'>
+    <img class="planetCardImg" src='${questionImg}'>
     <h3>Mystery</h3>
-    <p class="cardInfo4">Quick Overview of planets category</p>
+    <p class="cardInfo4">Click Here!</p>
     </div>
 
     </div>
